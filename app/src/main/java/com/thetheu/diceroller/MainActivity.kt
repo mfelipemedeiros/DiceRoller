79ea9f05748f274a0f265c24b5239e508527871d
Matheus Felipe <65515159+mfelipemedeiros@users.noreply.github.com>
2022-03-26 16:50:16 -0300

    add images

--- app/src/main/java/com/thetheu/diceroller/MainActivity.kt
+++ app/src/main/java/com/thetheu/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.thetheu.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
             val rollButton: Button = findViewById(R.id.roll_button)
             rollButton.setOnClickListener{ rollDice()}
 
+
     }
     private fun rollDice(){
         //Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
@@ -24,15 +26,19 @@ class MainActivity : AppCompatActivity() {
 
         resultText.text = randomInt.toString()
     */
-    val randomInt  = (1..6).random()
-    val drawableResource = when (randomInt){
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val randomInt  = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+
 
 
     }
